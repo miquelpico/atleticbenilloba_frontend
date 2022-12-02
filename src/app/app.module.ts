53dbf49e7d4e4d00eb33127bcb10ae7268da0792
miquelpico <miquelpico17@gmail.com>
2022-12-02 16:03:21 +0100

    Final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { JugadoresComponent } from './jugadores/jugadores.component';
-import { InicioComponent } from './inicio/inicio.component';
 import { Route, RouterModule } from '@angular/router';
-import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { BenvingudaComponent } from './benvinguda/benvinguda.component';
 
 const APP_ROUTES: Route[] = [
-  {path: '', component: InicioComponent},
-  {path: 'inicio', component: InicioComponent},
+  {path: '', component: BenvingudaComponent},
   {path: 'benvinguda', component: BenvingudaComponent},
-  {path: 'listado-plantilla', component: JugadoresComponent}
+  {path: 'plantilla', component: JugadoresComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     JugadoresComponent,
-    InicioComponent,
-    FooterComponent,
     HeaderComponent,
     BenvingudaComponent
   ],
